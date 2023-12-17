2eac2e4e51e25b7daa473c21ce416cb1bd23356f
Sven Roels <sven@MBP-van-Sven.localdomain>
2023-12-17 16:48:59 +0100

    End of Appendix B v2

--- frontend/web-app/app/lib/fetchWrapper.ts
+++ frontend/web-app/app/lib/fetchWrapper.ts
@@ -66,9 +66,9 @@ async function handleResponse(response: Response) {
     } else {
         const error = {
             status: response.status,
-            message: typeof data === 'string' ? data : response.statusText
+            message: typeof data === 'string' && data.length > 0 ? data : response.statusText
         }
-        return { error };
+        return {error};
     }
 }
 
